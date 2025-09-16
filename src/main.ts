19f07ba5f28f3102f313ca7bd5d0d226e93b11ef
Kostczev <churczev.k@gmail.com>
2025-09-16 17:00:03 +0500

    fix: change main.ts for base href in index.html

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,12 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
+const isGitHubPages = window.location.hostname.includes('github.io');
+const baseHref = isGitHubPages ? '/tik-talk/' : '/';
+
+const baseElement = document.createElement('base');
+baseElement.href = baseHref;
+document.head.prepend(baseElement);
+
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
